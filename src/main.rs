250f1d2fbb777ed4f2971c8af4083bbe77dbabe1
Alessandro Galli <a.galli85@gmail.com>
2024-03-26 22:51:55 +0100

    chore: variable shadowing type-change

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 
 fn main() {
-   let x = 5;
-   let x = x + 1;
+   let spaces = "     ";
+   println!("spaces contains '{}'", spaces);
 
-   {
-    let x = x * 2;
-    println!("the value of x in the inner scope is : {}", x);
-   }
-
-   println!("the value of x is : {}", x);
+   let spaces = spaces.len();
+   println!("spaces contains '{}'", spaces);
 }
