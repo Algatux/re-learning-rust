63881dcbe03bf4800181b359bad44f37dee93df8
Alessandro Galli <a.galli85@gmail.com>
2024-03-26 23:56:11 +0100

    chore: compound type usage - array, convertion to function

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 use std::io;
 
-fn main() {
-    let array = [1, 2, 3, 4, 5];
-
-    println!("Please enter an array index between 0..4 :");
-
+fn read_index() -> usize {
     let mut index = String::new();
 
     io::stdin()
         .read_line(&mut index)
         .expect("Failed to read line!");
 
-    let index: usize = index
+    index
         .trim()
         .parse()
-        .expect("Not a number!");
+        .expect("Not a number!")
+}
+
+fn main() {
+    let array = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index between 0..4 :");
+
+    let index = read_index();
 
     println!("The value of the element at index {} is : {}", index, array[index]);
 }
