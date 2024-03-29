15235701d65bb1858d39d828e42607be582aa5d2
Alessandro Galli <a.galli85@gmail.com>
2024-03-29 21:42:39 +0100

    chore: structs basics - method syntax

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 
 
-#[derive(Debug)]
 struct Person{
     name: String,
     surname: String
 }
 
+// Constructor block
+impl Person {
+    fn new(name: String, surname: String) -> Self {
+        Self {name, surname}
+    }
+}
+
+// Methods block
+impl Person {
+    fn complete_name(&self) -> String {
+        format!("{} {}", self.name, self.surname)
+    }
+
+    fn change_name(&mut self, new_name: String) {
+        self.name = new_name
+    }
+}
+
 fn main() {
-    let ale:Person = Person {
-        name: String::from("Alessandro"),
-        surname: String::from("Galli") 
-    };
+    let mut ale:Person = Person::new(String::from("Alessandro"),String::from("Galli"));
 
-    dbg!(&ale);
+    ale.change_name(String::from("Giovanni"));
 
-    println!("name: {}, surname: {}", ale.name, ale.surname);
-    println!("Person: {:?}", ale);
+    println!("{}", ale.complete_name());
 }
 
