215f110105952cf0fd2ead8f0ecc2af4cf663dfd
Alessandro Galli <a.galli85@gmail.com>
2024-03-29 21:15:41 +0100

    chore: structs basics - tuples

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,9 @@
 
 
-struct User {
-    name: String,
-    email: String,
-    age: u8
-}
+struct RGBColor(u8, u8, u8);
 
 fn main() {
-    let user = build_user(
-        String::from("Alessandro"),
-        String::from("fake@email.com"),
-        38
-    );
+    let red = RGBColor(255,0,0);
 
-    println!("Name: {}, Email: {}, Age {}", user.name, user.email, user.age);
-
-    let user2: User = User {
-        name: String::from("Valeria"),
-        ..user
-    };
-
-    println!("Name: {}, Email: {}, Age {}", user2.name, user2.email, user2.age);
-} 
-
-fn build_user(name: String, email: String, age: u8) -> User {
-    User { name, email, age }
+    print!("{}, {}, {}", red.0, red.1, red.2)
 }
