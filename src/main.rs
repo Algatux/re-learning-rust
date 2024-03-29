325dee3310e4405e30a92050f35bf0fcb2ddec7b
Alessandro Galli <a.galli85@gmail.com>
2024-03-29 21:06:31 +0100

    chore: structs basics - init shorthands

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,15 @@ struct User {
 }
 
 fn main() {
-    let user = User{
-        name: String::from("Alessandrio"),
-        email: String::from("fake@email.com"),
-        age: 38
-    };
+    let user = build_user(
+        String::from("Alessandro"),
+        String::from("fake@email.com"),
+        38
+    );
 
     println!("Name: {}, Email: {}, Age {}", user.name, user.email, user.age);
 } 
+
+fn build_user(name: String, email: String, age: u8) -> User {
+    User { name, email, age }
+}
