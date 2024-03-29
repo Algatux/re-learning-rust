70ad2db4855106ea0a0f7e5100e1f276a17b21ee
Alessandro Galli <a.galli85@gmail.com>
2024-03-29 21:09:21 +0100

    chore: structs basics - update syntax

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,13 @@ fn main() {
     );
 
     println!("Name: {}, Email: {}, Age {}", user.name, user.email, user.age);
+
+    let user2: User = User {
+        name: String::from("Valeria"),
+        ..user
+    };
+
+    println!("Name: {}, Email: {}, Age {}", user2.name, user2.email, user2.age);
 } 
 
 fn build_user(name: String, email: String, age: u8) -> User {
